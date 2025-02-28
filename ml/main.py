2c467f2d14cd10e643738f4db4a68f454925d53d
Arseny Bolotnikov <ars.bolotnikov93@gmail.com>
2025-02-28 22:04:17 +0300

    intermediate commit

--- ml/main.py
+++ ml/main.py
@@ -16,6 +16,8 @@ from sklearn.inspection import permutation_importance
 from sklearn.feature_selection import mutual_info_regression, f_regression
 from sklearn.decomposition import PCA
 import shap
+from collections.abc import Mapping
+import re
 
 
 # 1. SHAP Feature Importance
@@ -62,6 +64,32 @@ def gradient_boosting_feature_importance(X, y):
     gb_importance = gb_importance.sort_values(by='GB Importance', ascending=False)
     return gb_importance
 
+def read_extract_config(config):
+    number_with_unit_pattern = re.compile(r'^\s*(-?\d+(\.\d+)?)(.*)$')
+
+    extracted_data = []
+
+    def traverse(node, path=''):
+        if isinstance(node, dict):
+            for key, value in node.items():
+                new_path = f"{path}.{key}" if path else key
+                traverse(value, new_path)
+        elif isinstance(node, list):
+            for index, item in enumerate(node):
+                new_path = f"{path}[{index}]"
+                traverse(item, new_path)
+        elif isinstance(node, str):
+            match = number_with_unit_pattern.match(node)
+            if match:
+                number = float(match.group(1))
+                unit = match.group(3).strip()
+                full_path = f"{path}|{unit}" if unit else path  # Append unit if available
+                extracted_data.append({'path': full_path, 'value': number})
+        elif isinstance(node, (int, float)):
+            extracted_data.append({'path': path, 'value': node})
+
+    traverse(config)
+    return extracted_data
 
 def load_config(path: str):
     """
@@ -128,6 +156,9 @@ def calculate_average_tps(json_file):
 
 
 def objective_function():
+    # use core.yaml
+    subprocess.run('export FABRIC_CFG_PATH=core.yaml', shell=True)
+
     subprocess.run('cd ../fabric-samples/test-network && ./network.sh up createChannel -s couchdb', shell=True)
 
     #deploy chaincode
@@ -149,21 +180,25 @@ def objective_function():
 
 def fill_config(conf, next_point, sample_params):
     for i in range(len(sample_params)):
-        internal_path = sample_params[i].name.split('.')
-        parts = internal_path[-1].split('|')
-        assert(len(parts) > 0 and len(parts) <= 2)
-        internal_path[-1] = parts[0]
+        try:
+            internal_path = sample_params[i].name.split('.')
+            parts = internal_path[-1].split('|')
+            assert(len(parts) > 0 and len(parts) <= 2)
+            internal_path[-1] = parts[0]
 
-        assert(len(internal_path) > 0)
-        link = conf[internal_path[0]]
-        for j in range(1, len(internal_path)):
-            link = link[internal_path[j]]
-        # print(link)
-        if len(parts) == 1:
-            link = next_point[i]
-        else:
-            link = str(int(next_point[i])) + parts[1]
-        pass
+            assert(len(internal_path) > 0)
+            link = conf[internal_path[0]]
+            for j in range(1, len(internal_path)):
+                link = link[internal_path[j]]
+            # print(link)
+            if len(parts) == 1:
+                link = next_point[i]
+            else:
+                link = str(int(next_point[i])) + parts[1]
+            pass
+        except Exception as e:
+            print(f"Error: {e}")
+            continue
 
 
 def main():
@@ -171,13 +206,14 @@ def main():
     logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
     logger = logging.getLogger(__name__)
 
-    logger.info("Starting optimization process")
+    # logger.info("Starting optimization process")
 
     CONFIG_PATH = '../fabric-samples/test-network/configtx/configtx.yaml'
     conf = load_config(str(CONFIG_PATH))
+    conf_core = load_config('core.yaml')
 
     # assert(False, "Config path")
-    logger.info(f"Loaded configuration from {CONFIG_PATH}")
+    # logger.info(f"Loaded configuration from {CONFIG_PATH}")
 
     # TODO: automate config
 
@@ -192,6 +228,25 @@ def main():
         Integer(1, 50, name="Profiles.ChannelUsingRaft.Orderer.EtcdRaft.Options.MaxInflightBlocks"),
         Integer(1, 128, name="Profiles.ChannelUsingRaft.Orderer.EtcdRaft.Options.SnapshotIntervalSize|MB"),
     ]
+
+    # print(conf_core)
+    core_data = read_extract_config(conf_core)
+    # print(core_data)
+
+    for mapping in core_data:
+        name = mapping['path']
+        if "address" in name.lower():
+            continue
+        value = mapping['value']
+        print(name, value)
+        if isinstance(value, float):
+            sample_params.append(Real(value * 0.75, value * 1.5, name=name))
+        elif isinstance(value, int):
+            sample_params.append(Integer(value - 10, value + 10, name=name))
+
+    print(len(sample_params))
+    # print(sample_params, sep="\n")
+    # raise RuntimeError("Stop here")
 
     # TODO: orderer.type == BFT
 
@@ -210,12 +265,21 @@ def main():
 
     def fit(sample_params, optimizer, conf, scores_snr_path="scores_snr.txt", optimals_path="optimals.txt", scores_path="scores.txt") -> tuple[pd.DataFrame, pd.Series]:
 
+        # cleanup
+        if os.path.exists(scores_path):
+            os.remove(scores_path)
+        if os.path.exists(optimals_path):
+            os.remove(optimals_path)
+        if os.path.exists(scores_snr_path):
+            os.remove(scores_snr_path)
+
         scores = []
         optimals = []
         scores_snr = []
 
         df = pd.DataFrame(columns=[param.name.split('|')[0] for param in sample_params])
         y = pd.Series(name="Throughput (TPS)")
+        # TODO: take failed transactions into account
 
         for i in range(NUM_EPOCHS):
             next_point = optimizer.ask()
@@ -227,9 +291,13 @@ def main():
                 break
 
             fill_config(conf, next_point, sample_params)
+            fill_config(conf_core, next_point, sample_params) # should throw when given params for other configs
 
             with open(CONFIG_PATH, 'w') as f:
                 yaml.dump(conf, f)
+
+            with open('core.yaml', 'w') as f:
+                yaml.dump(conf_core, f)
 
             print(f"Next point to evaluate: {next_point}")
             optimals.append(next_point)
@@ -265,23 +333,20 @@ def main():
     with open("line_profiler_stats.txt", "w") as f:
         profiler.print_stats(stream=f)
 
-    shap_feature_importance()
+    # TODO: sensitivity analysis
+    # from sensitivity import SensitivityAnalyzer
 
-    shap_importance = shap_feature_importance(df, y)
-    rf_importance = random_forest_feature_importance(df, y)
-    lasso_importance = lasso_feature_importance(df, y)
-    perm_importance = permutation_feature_importance(df, y)
-    gb_importance = gradient_boosting_feature_importance(df, y)
+    shap_importance = shap_feature_importance(X=df, y=y)
+    rf_importance = random_forest_feature_importance(X=df, y=y)
+    lasso_importance = lasso_feature_importance(X=df, y=y)
+    perm_importance = permutation_feature_importance(X=df, y=y)
+    gb_importance = gradient_boosting_feature_importance(X=df, y=y)
 
     shap_importance.to_csv('shap_feature_importance.csv', index=False)
     rf_importance.to_csv('random_forest_feature_importance.csv', index=False)
     lasso_importance.to_csv('lasso_feature_importance.csv', index=False)
     perm_importance.to_csv('permutation_feature_importance.csv', index=False)
     gb_importance.to_csv('gradient_boosting_feature_importance.csv', index=False)
-
-    
-
-    
 
 
 
